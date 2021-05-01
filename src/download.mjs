ba309985eaccc61cb0ee547d02e52384974a5d2c
Alan Layng <alan@readersludlow.com>
2021-05-01 15:59:01 +0100

    use new form s3js with generator streams

--- src/download.mjs
+++ src/download.mjs
@@ -1,31 +1,22 @@
 import { resolve } from 'path'
 import { download as s3download } from 's3js'
 
-import Speedo from 'speedo'
-
 import report from './report.mjs'
 
 export default function download (url, file, { progress, limit }) {
   return s3download(url, file, {
-    onProgress: progress ? doProgress(file) : undefined,
+    onProgress: !!progress && doProgress(file),
     limit
   })
 }
 
 function doProgress (dest) {
   report('file.transfer.start', resolve(dest))
-  const speedo = new Speedo()
   const direction = 'downloaded'
-  return ({ bytes, total, done }) => {
-    speedo.update({ current: bytes, total })
-    report(`file.transfer.${done ? 'done' : 'update'}`, {
-      bytes,
-      percent: speedo.percent(),
-      total,
-      taken: speedo.taken(),
-      eta: speedo.eta(),
-      speed: speedo.rate(),
-      direction
-    })
+  return data => {
+    const { bytes, done, speedo } = data
+    const { total, percent, eta, taken, rate: speed } = speedo
+    const payload = { bytes, percent, total, taken, eta, speed, direction }
+    report(`file.transfer.${done ? 'done' : 'update'}`, payload)
   }
 }
--- src/upload.mjs
+++ src/upload.mjs
@@ -1,30 +1,21 @@
 import { upload as s3upload } from 's3js'
 
-import Speedo from 'speedo'
-
 import report from './report.mjs'
 
 export default function upload (file, url, { progress, limit }) {
   return s3upload(file, url, {
-    onProgress: progress ? doProgress(url) : undefined,
+    onProgress: !!progress && doProgress(url),
     limit
   })
 }
 
 function doProgress (url) {
   report('file.transfer.start', url)
-  const speedo = new Speedo()
   const direction = 'uploaded'
-  return ({ bytes, total, done }) => {
-    speedo.update({ current: bytes, total })
-    report(`file.transfer.${done ? 'done' : 'update'}`, {
-      bytes,
-      percent: speedo.percent(),
-      total,
-      taken: speedo.taken(),
-      eta: speedo.eta(),
-      speed: speedo.rate(),
-      direction
-    })
+  return data => {
+    const { bytes, done, speedo } = data
+    const { percent, total, taken, eta, rate: speed } = speedo
+    const payload = { bytes, percent, total, taken, eta, speed, direction }
+    report(`file.transfer.${done ? 'done' : 'update'}`, payload)
   }
 }
