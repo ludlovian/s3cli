cafdac21c69b11f9fc029238360c4f8061e3fea3
Alan Layng <alan@readersludlow.com>
2021-06-28 12:04:40 +0100

    2.0.8

--- dist/s3cli.mjs
+++ dist/s3cli.mjs
@@ -148,7 +148,7 @@ sql.deleteHash="DELETE FROM hash WHERE url = $url";
 sql.insertHash="INSERT INTO hash (url, mtime, \"size\", hash) VALUES ($url, datetime($mtime), $size, $hash) ON CONFLICT DO UPDATE SET mtime = excluded.mtime, \"size\" = excluded.\"size\", hash = excluded.hash";
 sql.insertSync="INSERT INTO sync (\"type\", path, url, mtime, \"size\") VALUES ($type, $path, $url, datetime($mtime), $size)";
 sql.selectChanged="SELECT src.url AS \"from\", dst.url AS \"to\" FROM sync src JOIN hash srcHash ON srcHash.url = src.url JOIN sync dst ON dst.path = src.path JOIN hash dstHash ON dstHash.url = dst.url WHERE src.\"type\" = 'src' AND dst.\"type\" = 'dst' AND srcHash.hash != dstHash.hash ORDER BY src.url";
-sql.selectHash="SELECT hash FROM hash WHERE url = $url AND mtime = $mtime AND \"size\" = $size";
+sql.selectHash="SELECT hash FROM hash WHERE url = $url AND mtime = datetime($mtime) AND \"size\" = $size";
 sql.selectMissingFiles="SELECT src.url AS url, src.path AS path FROM sync src LEFT JOIN sync dst ON src.path = dst.path AND dst.\"type\" = 'dst' WHERE src.\"type\" = 'src' AND dst.path IS NULL ORDER BY src.url";
 sql.selectMissingHashes="SELECT a.url AS url FROM sync a LEFT JOIN hash b ON a.url = b.url AND a.mtime = b.mtime AND a.\"size\" = b.\"size\" WHERE b.hash IS NULL ORDER BY a.url";
 sql.selectSurplusFiles="SELECT dst.url AS url FROM sync dst LEFT JOIN sync src ON src.path = dst.path AND src.\"type\" = 'src' WHERE dst.\"type\" = 'dst' AND src.path IS NULL ORDER BY dst.url";
@@ -953,7 +953,7 @@ async function sync (srcRoot, dstRoot, opts = {}) {
 }
 
 const prog = sade('s3cli');
-const version = '2.0.7';
+const version = '2.0.8';
 
 prog.version(version);
 
