fe582728fa3daf7daee8d3ba7003a34a34543205
Alan Layng <alan@readersludlow.com>
2021-07-13 22:38:04 +0100

    locate credentials file properly

--- src/drive/api.mjs
+++ src/drive/api.mjs
@@ -1,9 +1,15 @@
+import { fileURLToPath } from 'url'
+import { resolve } from 'path'
 import once from 'pixutil/once'
 
 const getDriveAPI = once(async function getDriveAPI () {
   const scopes = ['https://www.googleapis.com/auth/drive']
   const { default: driveApi } = await import('@googleapis/drive')
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = 'credentials.json'
+  const credentials = resolve(
+    fileURLToPath(import.meta.url),
+    '../../credentials.json'
+  )
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = credentials
   const auth = new driveApi.auth.GoogleAuth({ scopes })
   const authClient = await auth.getClient()
   return driveApi.drive({ version: 'v3', auth: authClient })
