072680670738a94e3818d223076aea46b73f8798
Alan Layng <alan@readersludlow.com>
2021-07-15 10:07:55 +0100

    hardcode credentials location to make it easier

--- src/drive/api.mjs
+++ src/drive/api.mjs
@@ -1,15 +1,12 @@
-import { fileURLToPath } from 'url'
-import { resolve } from 'path'
 import once from 'pixutil/once'
+
+const PROJECT_DIR = '/home/alan/dev/s3cli/'
+const CREDENTIALS = PROJECT_DIR + 'credentials.json'
 
 const getDriveAPI = once(async function getDriveAPI () {
   const scopes = ['https://www.googleapis.com/auth/drive']
   const { default: driveApi } = await import('@googleapis/drive')
-  const credentials = resolve(
-    fileURLToPath(import.meta.url),
-    '../../credentials.json'
-  )
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = credentials
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = CREDENTIALS
   const auth = new driveApi.auth.GoogleAuth({ scopes })
   const authClient = await auth.getClient()
   return driveApi.drive({ version: 'v3', auth: authClient })
